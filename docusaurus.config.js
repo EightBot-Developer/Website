8c7da959d04927bbf5a7dfdc7218c57f5f3c5c08
gx1285 <78240988+gx1285@users.noreply.github.com>
2022-10-07 20:03:01 +0900

    fix

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -9,7 +9,7 @@ const config = {
   title: 'My Site',
   tagline: 'Dinosaurs are cool',
   url: 'https://eightbot-developer.github.io',
-  baseUrl: 'Website/',
+  baseUrl: '/Website/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
