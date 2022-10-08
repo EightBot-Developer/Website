c22e5eeb7b09341d3f56cb42ae72f28c5b240845
gx1285 <78240988+gx1285@users.noreply.github.com>
2022-10-08 10:03:51 +0900

    copyright

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -125,7 +125,7 @@ const config = {
             ],
           },
         ],
-        copyright: `Copyright © ${new Date().getFullYear()} My Project, Inc. Built with Docusaurus.`,
+        copyright: `Copyright © ${new Date().getFullYear()} EightBot Developers`,
       },
       prism: {
         theme: lightCodeTheme,
