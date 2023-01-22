629dc192bb34bf9698fc2d2cb05d73874537d5f0
gx1285 <78240988+gx1285@users.noreply.github.com>
2023-01-22 20:27:34 +0900

    2.4.0 beta

--- src/components/HomepageFeatures/index.js
+++ src/components/HomepageFeatures/index.js
@@ -23,12 +23,11 @@ const FeatureList = [
     ),
   },
   {
-    title: 'Powered by React',
+    title: '便利機能が少しづつ増えている',
     Svg: require('@site/static/img/undraw_docusaurus_react.svg').default,
     description: (
       <>
-        Extend or customize your website layout by reusing React. Docusaurus can
-        be extended while reusing the same header and footer.
+        あまり必要ではない機能から必要な機能まであり、これらの機能は日々追加されています。
       </>
     ),
   },
